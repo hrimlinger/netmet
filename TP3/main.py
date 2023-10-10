ed78859a71afb59e25d79a80a5b07172e9ae6316
hrimlinger <hugorimlinger4@gmail.com>
2023-10-10 08:42:07 +0200

    tp3

--- TP3/main.py
+++ TP3/main.py
@@ -66,6 +66,7 @@ if __name__ == "__main__":
     ip_addresses_list = [
         {"address_v4": "142.250.201.4"},
         {"address_v4": "157.240.202.35"},
+        {"address_v4": "104.16.124.96"},
         {"address_v4": "142.250.188.228"},
     ]
 
